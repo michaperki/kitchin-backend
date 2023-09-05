633065983ee972039519183423fdcecccd38c005
Michael Perkins <mperkins1995@gmail.com>
2023-09-05 13:14:39 -0500

    Switch dotenv to python-dotenv

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-from dotenv import load_dotenv
+from python-dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
