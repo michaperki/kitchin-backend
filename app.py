d537cf4bbe78880299eb5ff8db40de79b5fbc3c8
Michael Perkins <mperkins1995@gmail.com>
2023-09-05 12:28:52 -0500

    Update Flask PORT config

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+# Use the PORT environment variable provided by Heroku
+port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=port)
