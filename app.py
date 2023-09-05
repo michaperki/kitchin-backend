2d74774a25e3afc25371b45099a3f07cef14530f
Michael Perkins <mperkins1995@gmail.com>
2023-09-05 15:46:39 -0500

    Add CORS routing

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
